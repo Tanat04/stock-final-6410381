3425546ad3152885dc7d4608bc30529ac4c22b92
Tanat04 <tanat.navin@gmail.com>
2023-03-18 18:52:47 +0700

    Add Read, Create and Delate function

---
+++ src/pages/supplier/index.jsx
@@ -0,0 +1,73 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+export default function Home({ suppliers }) {
+
+  function deletesupplier(id) {
+    fetch(`/api/stockFinal/suppliers/${id}`,
+      {
+        method: 'DELETE'
+      })
+      .then(res => res.json())
+      .then(data => {
+        // alert("Deleting " + id)
+        window.location.reload(false);
+      })
+
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Suppliers Management</title>
+      </Head>
+      <h1>suppliers</h1>
+          <p style={{ margin: '0.4rem' }}>
+            <Link href="/supplier/supplier">+New Supplier</Link>
+          </p>
+      <table>
+        <thead>
+          <tr>
+            <th style={{width: '20rem'}}>Supplier Name</th>
+            <th style={{width: '10rem'}}>Address</th>
+            <th style={{width: '10rem'}}>Phone Number</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            suppliers.map(supplier => {
+              return (
+                <tr key={supplier._id}>
+                  <td >
+                    <Link href={`/supplier/${supplier._id}`}>
+                      {supplier.supplierName}
+                    </Link>
+                  </td>
+                  <td style={{textAlign:'center'}}>{supplier.address}</td>
+                  <td style={{textAlign:'center'}}>{supplier.phoneNumber}</td>
+                  <td>                      
+                      <>
+                        <Link href={`/supplier/update/${supplier._id}`}>Update</Link>
+                        &nbsp;&nbsp;&nbsp;
+                        <button onClick={() => deletesupplier(supplier._id)}>Delete</button>
+                      </>
+                  </td>
+                </tr>
+              )
+            })
+          }
+        </tbody>
+      </table>
+      <hr/>
+      <Link href="/">Home</Link>
+      <p>
+      </p>
+
+    </>
+  )
+}
+export async function getServerSideProps() {
+  const res = await fetch(`https://stock-final-6410381.vercel.app/api/stockFinal/supplier`)
+  const suppliers = await res.json()
+  return { props: { suppliers } }
+}
---
+++ src/pages/supplier/supplier.jsx
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import Link from "next/link";
+
+export default function AddSupplier() {
+  const { register, handleSubmit } = useForm();
+  const [data, setData] = useState("");
+
+  const saveSupplier = async (data) => {
+    const response = await fetch("/api/stockFinal/suppliers", {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, *cors, same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      // credentials: "same-origin", // include, *same-origin, omit
+      headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      // redirect: "follow", // manual, *follow, error
+      referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      // serialisation
+      body: JSON.stringify(data), // body data type must match "Content-Type" header
+    });
+    const result = await response.json(); // deserialise
+    if (result.error) {
+      alert("Error: " + result.error);
+    } else {
+      alert("Supplier Added");
+      window.location.href = "/supplier";
+    }
+    console.log(result);
+    setData(JSON.stringify(data));
+  };
+
+  return (
+    <div style={{ margin: "1rem" }}>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/supplier">Suppliers</Link>
+          </li>
+        </ul>
+      </nav>
+      <form onSubmit={handleSubmit(saveSupplier)}>
+        <h1>New Supplier</h1>
+        <label htmlFor="supplierName">Supplier Name</label>
+        <br />
+        <input
+          id="supplierName"
+          {...register("supplierName", { required: true })}
+          placeholder="Tanat Arora"
+        />
+        <br />
+
+        <label htmlFor="address">Address</label>
+        <br />
+        <input
+          id="address"
+          {...register("address", { required: true })}
+          placeholder="123 Main St."
+        />
+        <br />
+
+        <label htmlFor="phoneNumber">Phone Number</label>
+        <br />
+        <input
+          id="phoneNumber"
+          {...register("phoneNumber", { required: true })}
+          placeholder="555-123-1234"
+        />
+        <br />
+
+        <input type="submit" />
+        <p>{data}</p>
+        <br />
+      </form>
+    </div>
+  );
+}
